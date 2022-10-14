c0a17abd6a30d9b9cc21a8d67fc5d1fb39269ecf
John-John Tedro <udoprog@tedro.se>
2022-10-14 17:55:26 +0200

    Reintroduce map_feature and move empty

---
+++ tests/map_feature.rs
@@ -0,0 +1,38 @@
+#![cfg(feature = "map")]
+
+use fixed_map::{Key, Map};
+
+#[derive(Clone, Copy, Key)]
+enum Part {
+    One,
+    Two,
+}
+
+#[derive(Clone, Copy, Key)]
+enum Key {
+    Simple,
+    Composite(Part),
+    String(&'static str),
+    Number(u32),
+    Singleton(()),
+}
+
+#[test]
+fn map_feature() {
+    let mut map = Map::new();
+
+    map.insert(Key::Simple, 1);
+    map.insert(Key::Composite(Part::One), 2);
+    map.insert(Key::String("foo"), 3);
+    map.insert(Key::Number(1), 4);
+    map.insert(Key::Singleton(()), 5);
+
+    assert_eq!(map.get(Key::Simple), Some(&1));
+    assert_eq!(map.get(Key::Composite(Part::One)), Some(&2));
+    assert_eq!(map.get(Key::Composite(Part::Two)), None);
+    assert_eq!(map.get(Key::String("foo")), Some(&3));
+    assert_eq!(map.get(Key::String("bar")), None);
+    assert_eq!(map.get(Key::Number(1)), Some(&4));
+    assert_eq!(map.get(Key::Number(2)), None);
+    assert_eq!(map.get(Key::Singleton(())), Some(&5));
+}
